28856cbd0ad1a38add935460dd9cccd2a0dc676b
George Soulantikas <soulgeo01@gmail.com>
2026-05-22 15:07:07 +0300

    remove generated obstacles

--- src/main.cpp
+++ src/main.cpp
@@ -24,20 +24,6 @@ int main() {
     camera.projection = CAMERA_PERSPECTIVE;
     float cameraDistance = 3.0f;
 
-    // Obstacle generation
-    float heights[MAX_COLUMNS] = {0};
-    Vector3 positions[MAX_COLUMNS] = {0};
-    Color colors[MAX_COLUMNS] = {0};
-
-    for (int i = 0; i < MAX_COLUMNS; i++) {
-        heights[i] = (float)GetRandomValue(1, 12);
-        positions[i] = (Vector3){(float)GetRandomValue(-15, 15), heights[i] / 2.0f, (float)GetRandomValue(-15, 15)};
-        colors[i] = (Color){
-            (unsigned char)GetRandomValue(20, 255), 
-            (unsigned char)GetRandomValue(10, 55), 
-            30, 255};
-    }
-
     SetTargetFPS(60);
 
     //================================================================================== 
@@ -45,7 +31,10 @@ int main() {
     while (!WindowShouldClose())
     {
         game.update();        
-        
+
+        // =============================================================================
+        // Camera
+
         // Extract orientation and tracking data straight from GameObject methods
         Vector3 currentPosition = playerShip->getPosition();
         Vector3 forward = playerShip->getForward();
@@ -63,18 +52,7 @@ int main() {
         BeginDrawing();
             ClearBackground(BLACK);
             BeginMode3D(camera);
-                DrawPlane((Vector3){0.0f, 0.0f, 0.0f}, (Vector2){32.0f, 32.0f}, DARKGRAY);
-                DrawCube((Vector3){-16.0f, 2.5f, 0.0f}, 1.0f, 5.0f, 32.0f, BLUE);
-                DrawCube((Vector3){16.0f, 2.5f, 0.0f}, 1.0f, 5.0f, 32.0f, LIME);
-                DrawCube((Vector3){0.0f, 2.5f, 16.0f}, 32.0f, 5.0f, 1.0f, GOLD);
-
-                for (int i = 0; i < MAX_COLUMNS; i++) {
-                    DrawCube(positions[i], 2.0f, heights[i], 2.0f, colors[i]);
-                    DrawCubeWires(positions[i], 2.0f, heights[i], 2.0f, MAROON);
-                }
-
                 game.draw();
-
             EndMode3D();
         EndDrawing();
     }
